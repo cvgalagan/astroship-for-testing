6256f24e2ea4be90a58776b3fb4881ac61df4e34
cvgalagan <cvgalagan@gmail.com>
2024-08-03 15:41:10 +0300

    Added timers input

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import GridPage from './components/GridPage/GridPage'
 
 function App() {
   const [timersCount, setTimersCount] = useState(100);
+  const [newTimersCount, setNewTimersCount] = useState(50); // Состояние для ввода количества новых таймеров
 
   const addTimers = () => {
-    setTimersCount((prevCount) => prevCount + 50);
+    setTimersCount((prevCount) => prevCount + newTimersCount);
   };
 
   const removeTimers = () => {
-    setTimersCount((prevCount) => Math.max(prevCount - 50, 0));
+    setTimersCount((prevCount) => Math.max(prevCount - newTimersCount, 0));
   };
-
 
   return (
     <>
       <div style={{ marginBottom: '20px' }}>
-        <button onClick={addTimers}>Добавить 50 таймеров</button>
-        <button onClick={removeTimers}>Удалить 50 таймеров</button>
+      <label>
+          Количество таймеров:
+          <input 
+            type="number" 
+            value={newTimersCount} 
+            onChange={(e) => setNewTimersCount(Number(e.target.value))} 
+            style={{ marginLeft: '10px', marginRight: '10px' }}
+          />
+        </label>
+
+        <button onClick={addTimers}>Добавить {newTimersCount} таймеров</button>
+        <button onClick={removeTimers}>Удалить {newTimersCount} таймеров</button>
       </div>
       <GridPage timersCount={timersCount} />
     </>
