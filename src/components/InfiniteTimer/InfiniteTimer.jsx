e126a8442006fdcf9a6a980a920f93d9c1c8a81d
cvgalagan <cvgalagan@gmail.com>
2024-08-03 23:06:03 +0300

    Disabled timer

--- src/components/InfiniteTimer/InfiniteTimer.jsx
+++ src/components/InfiniteTimer/InfiniteTimer.jsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 const InfiniteTimer = () => {
-  const [count, setCount] = useState(0);
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
+//   useEffect(() => {
+//     // const timerId = setInterval(() => {
+//     //   setCount((prevCount) => prevCount + 1);
+//     // }, 1000);
 
-    // Очистка таймера при размонтировании компонента
-    return () => clearInterval(timerId);
-  }, []);
+//     // // Очистка таймера при размонтировании компонента
+//     // return () => clearInterval(timerId);
+//   }, []);
 
   return (
     <div>
-      <p>Счетчик: {count}</p>
+      <p>Счетчик: {0}</p>
     </div>
   );
 };
